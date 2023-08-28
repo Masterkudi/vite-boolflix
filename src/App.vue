981a35058a0d7e851e7ecd1431132462b8a0a451
masterkudi <cudini.andrea@gmail.com>
2023-08-28 21:38:49 +0200

    provo a creare MoviesList.vue per importare le API al momento non funziona

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <script>
-import TheHeader from './components/TheHeader.vue';
-import TheFooter from './components/TheFooter.vue';
+import TheHeader from "./components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
+import MoviesList from "./components/MoviesList.vue";
 
 export default {
   components: {
     TheHeader,
     TheFooter,
+    MoviesList
   }
 }
 </script>
 
 <template>
 <TheHeader></TheHeader>
-<main></main>
+<main>
+  <div class="gallery">
+        <div class="row row-cols-6">
+          <div class="col" v-for="(character, i) in charactersList" :key="`character_id`">
+            <div class="card border-0 rounded-0 h-100 bg-transparent text-white">
+              <img :src="character.thumb" alt="" />
+              <div class="card-body p-0 pb-5 pt-2">
+                {{ character.series }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</main>
 <TheFooter></TheFooter>
 </template>
 
---
+++ src/components/MoviesList.vue
@@ -0,0 +1,66 @@
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            movies: [],
+        };
+    },
+    methods: {
+        fetchMovies() {
+            const url = "https://developer.themoviedb.org/reference/search-movie?api_key=12eb07f498add0bf766c500177706cec";
+
+            axios.get(url).then((response) => {
+                this.movies = response.data.data;
+                console.log(this.movies)
+        });
+        },
+    },
+    mounted() {
+        this.fetchMovies();
+    },
+};
+</script>
+
+<template>
+    <div class="row row-cols-5">
+       
+        <div class="col" v-for="movie in movies" :key="movie.id">
+            <div class="card rounded-0">
+                <img :src="movie.card_images[0].image_url" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ movie.name }}</h5>
+                    <p class="card-text">
+                        {{ movie.archetype }}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+.card {
+    background-color: #18181A;
+    height: 200px;
+}
+.card-title {
+    color: #fff;
+    font-size: .8rem;
+}
+.card-text {
+    font-size: .6rem;
+}
+.col {
+    padding-left: .5rem;
+    padding-right: .5rem;
+    padding-bottom: .5rem;
+}
+
+.card-img-top {
+    width: 50%;
+}
+
+</style>
