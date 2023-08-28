066e22ef3fb4c9fc03f2a1e26d113140221323f8
masterkudi <cudini.andrea@gmail.com>
2023-08-28 21:21:48 +0200

    header + logo + searchbar

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -6,9 +6,18 @@ export default {
 </script>
 
 <template>
-
+    <div class="d-flex text-align-center text-danger p-3 bg-black justify-content-between">
+        <h1 class="logo mb-0">BOOLFLIX</h1>
+        <nav class="navbar bg-body-black">
+            <div class="container-fluid">
+                <a class="navbar-brand"></a>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2 text-white bg-dark border-0" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn text-white bg-primary" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    </div>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
