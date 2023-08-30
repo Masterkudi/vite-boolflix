8124ea363cc8a6746f6d080c847a25df6e48a914
masterkudi <cudini.andrea@gmail.com>
2023-08-30 09:57:11 +0200

    aggiornamento header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -6,31 +6,30 @@ export default {
 </script>
 
 <template>
-    <div class="d-flex text-align-center text-danger p-3 bg-black justify-content-between">
-        <h1 class="logo mb-0">BOOLFLIX</h1>
+    <div class="d-flex p-3 bg-black justify-content-between">
+        <h1 class="logo align-self-center text-danger  mb-0">BOOLFLIX</h1>
+        <ul class="d-flex flex-row align-self-center justify-content-between navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item text-white">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item text-white">
+                <a class="nav-link" href="#">Film</a>
+            </li>
+            <li class="nav-item text-white">
+                <a class="nav-link" href="#">Serie TV</a>
+            </li>
+            <li class="nav-item text-white">
+                <a class="nav-link" href="#">Originali</a>
+            </li>
+            <li class="nav-item text-white">
+                <a class="nav-link" href="#">Aggiunti di recente</a>
+            </li>
+            <li class="nav-item text-white">
+                <a class="nav-link" href="#">La mia lista</a>
+            </li>
+        </ul>
         <nav class="navbar bg-body-black">
             <div class="container-fluid">
-                <a class="navbar-brand"></a>
-                <ul class="d-flex flex-row justify-content-start navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item text-white">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
-                    <li class="nav-item text-white">
-                        <a class="nav-link" href="#">Film</a>
-                    </li>
-                    <li class="nav-item text-white">
-                        <a class="nav-link" href="#">Serie TV</a>
-                    </li>
-                    <li class="nav-item text-white">
-                        <a class="nav-link" href="#">Originali</a>
-                    </li>
-                    <li class="nav-item text-white">
-                        <a class="nav-link" href="#">Aggiunti di recente</a>
-                    </li>
-                    <li class="nav-item text-white">
-                        <a class="nav-link" href="#">La mia lista</a>
-                    </li>
-                </ul>
                 <form class="d-flex" role="search">
                     <input class="form-control me-2 text-white bg-dark border-0" v-model="searchQuery" @input="filterMovies"
                         type="search" placeholder="Search">
